88712df270500b56cfda590ad898843f6396af31
Aaren Cordova <aaren@gethomesome.com>
2021-05-17 14:53:58 -0700

    Added header

--- src/App.js
+++ src/App.js
@@ -2,22 +2,33 @@ import './App.scss';
 import Header from './Header';
 import {useState, useEffect} from 'react';
 import Storages from 'js-storage';
-
+import classNames from 'classnames';
+import 'antd/dist/antd.css';
 const storageNamespace = Storages.initNamespaceStorage('ron-swanson-quote-generator');
 
-
 function App() {
-    const [quoteCollection, setQuoteCollection] = useState([]);
-    console.log(storageNamespace)
+    const [totalQuotes, setTotalQuotes] = useState(10);
+    const [theme, setTheme] = useState('light');
+    const [quotes, setQuotes] = useState([]);
+    const [filteredQuotes, setFilteredQuotes] = useState([]);
+
     useEffect(() => {
         (async () => {
-
+            // https://ron-swanson-quotes.herokuapp.com/v2/quotes/10
         })();
     });
 
+    const isLightTheme = theme === 'light';
     return (
-        <div className="App">
-            <Header numVotes={3}/>
+        <div className={classNames('App', {'theme--light': isLightTheme, 'theme--dark': !isLightTheme})}>
+            <Header
+                theme={theme}
+                setTheme={setTheme}
+                quotes={quotes}
+                setFilteredQuotes={setFilteredQuotes}
+                totalQuotes={totalQuotes}
+                setTotalQuotes={setTotalQuotes}
+            />
         </div>
     );
 }
--- src/Header/index.js
+++ src/Header/index.js
@@ -1,10 +1,26 @@
 import './styles.scss';
+import classNames from 'classnames';
 
-export default function Header({numVotes}){
+
+export default function Header(
+    {
+        className,
+        quotes,
+
+    },
+) {
+
     return (
-        <header className='Header'>
-            <h3 className='Header__title'>Ron Swanson Voter</h3>
-            <h4 className='Header__subtitle'>&quot;Vote for your favorite quote!&quot;</h4>
+        <header className={classNames('Header', className)}>
+            <section className="Header__section">
+                <h3 className="Header__title">
+                    Ron Swanson Voter
+                    <span className="Header__votes">({quotes.length})</span>
+                </h3>
+                <h4 className="Header__subtitle">&quot;Vote for your favorite quote!&quot;</h4>
+            </section>
+
+
         </header>
     );
 }
